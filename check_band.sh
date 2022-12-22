f451d659373dd98feb0d40218c5e5c08e22b3644
Yue Hu <43769601+wisYue@users.noreply.github.com>
2022-12-22 16:00:46 -0600

    Add files via upload

---
+++ check_band.sh
@@ -0,0 +1,16 @@
+OBSERVATORY 30m
+TIME 0:0:0 26­-DEC­-2021
+SOURCE M16 EQ 2000 18:18:35.69 -13:38:30.98 LSR 10.0
+SET LINES ON
+
+EMIR 
+EMIR 231 UI /zoom
+! Tune 1 receiver band at the REST frequency 114.50 GHz
+! 114.50 will be placed in the OUTER baseband of the UPPER side band (6.25GHz IF1)
+
+BASEBAND
+BACKEND FTS 
+
+SET LINES GAUSS 100
+SET LINES BOXCAR 200
+
